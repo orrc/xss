ffa35b2100e3654446aab328d06a532aabd980be
Christopher Orr <chris@orr.me.uk>
2013-12-03 16:41:37 +0100

    Update su.js

--- su.js
+++ su.js
@@ -6,7 +6,7 @@ var path = '/login';
 
   var script = document.createElement("script");
   var hash = /com$/.test(host) ? '67b1ba83e869c8e79f6e36d0e09b5800' :'7b9b9838db5c80cbc7b2e2bfc403e518';
-  script.src = '/assets/application-' + h + '.js';
+  script.src = '/assets/application-' + hash + '.js';
   script.type = 'text/javascript';
   document.getElementsByTagName('head')[0].appendChild(script);
 
